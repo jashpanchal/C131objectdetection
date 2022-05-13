bce71c65a6ca3d666a5d7dca460a4eb869095123
shukurenai09 <78141688+jashpanchal@users.noreply.github.com>
2022-05-13 15:57:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 img = "";
+status = "";
+objects = [];
 
 function preload(){
     img = loadImage("dog_cat.jpg");
@@ -7,13 +9,39 @@ function preload(){
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();   
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status_").innerHTML = "Status: Detecting Objects";
 }
 
 function draw(){
     image(img,0,0,640,420);
-    fill("#ff0000");
-    text('Dog',70,70);
-    noFill();
-    stroke("#ff0000");
-    rect(60,50,350,350);
+    if(status != ""){
+        
+        for(i=0; i < objects.length; i++){
+            document.getElementById("status_").innerHTML = "Status: Object Detected";
+            fill("#ff0000");
+            percentage = floor(objects[i].confidence*100);
+            stroke("#ff0000");
+            text(objects[i].label + " " + percentage + "%",objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height-20);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("CocoSSD is initialized!!");
+    status = true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+
+    }  
 }
